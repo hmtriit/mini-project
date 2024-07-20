b8646fc258e026bf308923ae43c0d5ceb99b6c8d
hmtri <hmtri.it@gmail.com>
2024-07-20 22:51:33 +0700

    Encoder Password and matching Bcrypt, Create new Api Authentication (login)

--- identity-service/src/main/java/com/dev/miniprj/identityservice/exception/ErrorCode.java
+++ identity-service/src/main/java/com/dev/miniprj/identityservice/exception/ErrorCode.java
@@ -10,8 +10,8 @@ public enum ErrorCode {
     INVALID_KEY(1001, "Invalid message key"),
     USER_EXISTED(1002, "User existed"),
     USERNAME_INVALID(1003, "Username must be at least 1 characters"),
-    INVALID_PASSWORD(1004, "Password must be at least 8 characters")
-
+    INVALID_PASSWORD(1004, "Password must be at least 8 characters"),
+    USER_NOT_EXISTED(1005, "User not existed")
     ;
     private int code;
     private String message;
